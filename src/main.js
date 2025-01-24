a6d7a168163fb1b38256082ca1be037a005bc6fe
VitaliiZvieriev <zverew91@gmail.com>
2025-01-24 11:02:06 +0200

    fix pagination

--- src/main.js
+++ src/main.js
@@ -15,12 +15,14 @@ const simplelightboxOptions = {
 };
 iziToast.settings({
   position: 'topCenter',
-  timeout: 3500,
+  timeout: 4500,
   transitionIn: 'bounceInDown',
 });
 
-new SimpleLightbox('.gallery .gallery-link', simplelightboxOptions);
-
+const gallerySLB = new SimpleLightbox(
+  '.gallery .gallery-link',
+  simplelightboxOptions
+);
 const formEl = document.querySelector('.js-search-form');
 const loaderEl = document.querySelector('.js-loader');
 const galleryEL = document.querySelector('.js-gallery');
@@ -48,7 +50,7 @@ async function onSubmit(evt) {
   is_Loader_LoadMoreBtn_Gallery_Show(true, false, false);
 
   try {
-    urlParams.quary = userQuery;
+    urlParams.query = userQuery;
     urlParams.page = 1;
 
     const { data } = await searchHendler(urlParams);
@@ -58,13 +60,20 @@ async function onSubmit(evt) {
         message:
           'Sorry, there are no images matching your search query. Please try again!',
       });
+      formEl.reset();
       is_Loader_LoadMoreBtn_Gallery_Show();
       return;
     }
 
     galleryEL.innerHTML = createCardsTemplate(data.hits);
+    gallerySLB.refresh();
 
     formEl.reset();
+
+    if (data.total <= 15) {
+      is_Loader_LoadMoreBtn_Gallery_Show(false, false, true);
+      return;
+    }
 
     is_Loader_LoadMoreBtn_Gallery_Show(false, true, true);
   } catch (err) {
@@ -76,14 +85,32 @@ async function onSubmit(evt) {
   }
 }
 
-async function onLoadMore(evt) {
+async function onLoadMore() {
   is_Loader_LoadMoreBtn_Gallery_Show(true, false, true);
 
   urlParams.page++;
 
   try {
     const { data } = await searchHendler(urlParams);
+
     galleryEL.insertAdjacentHTML('beforeend', createCardsTemplate(data.hits));
+    gallerySLB.refresh();
+
+    scrollBy({
+      top:
+        document
+          .querySelector('.gallery-item:last-child')
+          .getBoundingClientRect().height * 2,
+      behavior: 'smooth',
+    });
+
+    if (data.total <= urlParams.page * 15) {
+      iziToast.info({
+        message: "We're sorry, but you've reached the end of search results.",
+      });
+      is_Loader_LoadMoreBtn_Gallery_Show(false, false, true);
+      return;
+    }
   } catch (err) {
     console.error(err);
     iziToast.error({
@@ -96,9 +123,9 @@ async function onLoadMore(evt) {
 }
 
 function is_Loader_LoadMoreBtn_Gallery_Show(
-  loader = true,
-  loadMore = true,
-  gallery = true
+  loader = false,
+  loadMore = false,
+  gallery = false
 ) {
   loader
     ? loaderEl.classList.remove('visually-hidden')
